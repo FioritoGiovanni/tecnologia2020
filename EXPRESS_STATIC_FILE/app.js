c669da5c91337fd948c1849cbf5c910ac1f70412
FioritoGiovanni <fiorito.giovanni@iisgalvanimi.edu.it>
2020-11-09 10:34:37 +0000

    fine 1.1

---
+++ EXPRESS_STATIC_FILE/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express(); 
+const path = require('path');
+//assuming app is express Object.
+app.get('/', function(req,res) {
+ res.sendFile('/workspace/tecnologia2020/EXPRESS_STATIC_FILE/' ,'index.html');
+});
+
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname ,'about.html')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/sitemap', function(req,res) {
+ res.sendFile(path.join(__dirname,'sitemap.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
